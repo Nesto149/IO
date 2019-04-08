28e38e33efb9aac89a3877b423ee203d3aeaaaa6
Mateusz Żurowski <51670@student.ue.poznan.pl>
2019-04-08 18:02:24 +0200

    podsumowanie

--- kalkulator.py
+++ kalkulator.py
@@ -2,9 +2,9 @@ print("to jest prosty kalkulator")
 <<<<<<< HEAD
 def dodaj (a,b):
 	return  a+b
-x = int(input())
-y = int(input())
-print(dodaj(x, y))
+# x = int(input())
+# y = int(input())
+# print(dodaj(x, y))
 print('Papa')
 print('Koniec programu')
 =======
